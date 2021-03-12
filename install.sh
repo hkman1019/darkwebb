c32565bca6510cedbf9117fe0ba79ae54de6cf0e
hkman1019 <71018012+hkman1019@users.noreply.github.com>
2021-03-12 19:41:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 apt update && apt upgrade -y -f
 apt install jq -y
 
-wget -q https://raw.githubusercontent.com/PHCitizen/v2ray-uuid-manager/main/phcv2raymanager
+wget -q https://raw.githubusercontent.com/hkman1019/darkwebb/master/jayrmanager
 mv jayrmanager /usr/local/sbin/jayrmanager
 chmod +x /usr/local/sbin/jayrmanager
 clear
 echo "========================================"
-echo "use phcv2raymanager to manage your UUID"
+echo "use jayrmanager to manage your UUID"
 echo "========================================"
